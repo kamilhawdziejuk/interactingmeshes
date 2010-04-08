36890320220a2fa7e001d11aa9c8cfb6b182bc53
kamil.hawdziejuk <kamil.hawdziejuk@f0349f97-431d-c3b9-9225-2ecc7eab64a9>
2010-04-08 21:19:14 +0000

    Core file

---
+++ Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.DirectX;
+
+namespace InteractingMeshes
+{
+    /// <summary>
+    /// A camera
+    /// </summary>
+    public class Camera
+    {
+        public Vector3 Position = new Vector3(0.0f, 2.0f, -25.0f);
+
+        public Vector3 LookAtPoint = new Vector3(0, 0, 0);
+
+        public Vector3 UpVector = new Vector3(0,1,0);
+    }
+}
